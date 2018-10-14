e804fa7f23e957f5fb0a753faf4bddb55b083a15
gaodawei <dwgao@alauda.io>
2018-10-15 02:28:38 +0800

    添加Feign配置

--- src/main/java/io/alauda/microservice/config/FeignClientConfig.java
+++ src/main/java/io/alauda/microservice/config/FeignClientConfig.java
@@ -1,8 +1,26 @@
 package io.alauda.microservice.config;
 
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
+@EnableCircuitBreaker
+@EnableFeignClients(basePackages = "io.alauda.**.*")
 @Configuration
 public class FeignClientConfig {
 
+    @Bean
+    public OkHttpClient okHttpClient(){
+        return new okhttp3.OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool())
+                .build();
+    }
 }
--- src/main/java/io/alauda/microservice/config/MicroServiceAutoConfiguration.java
+++ src/main/java/io/alauda/microservice/config/MicroServiceAutoConfiguration.java
@@ -1,9 +1,11 @@
 package io.alauda.microservice.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@EnableEurekaClient
 @Import({
         TokenConfig.class,
         JwtFilterConfig.class,
